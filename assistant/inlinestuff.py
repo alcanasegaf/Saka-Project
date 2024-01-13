433fa10ffccb1e752395f130fcac04b5461788b9
alcanasegaf <143788192+alcanasegaf@users.noreply.github.com>
2024-01-13 11:11:06 +0700

    update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -14,7 +14,7 @@ from . import *
 SUP_BUTTONS = [
     [
         Button.url("• Repo •", url="https://github.com/alcanasegaf/Saka-Userbot"),
-        Button.url("• Support •", url="t.me/kynansupport"),
+        Button.url("• Support •", url="t.me/alsuport"),
     ],
 ]
 
